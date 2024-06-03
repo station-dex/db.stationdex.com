8f8a6679753ab33b1f1d696b69bd0b2146d497a3
keyrunpay <tokeyrun@gmail.com>
2024-06-03 18:12:04 +0545

    get explorer home & stats function, get stablecoin value added

---
+++ sql/explorer/00-functions/get_explorer_home.sql
@@ -0,0 +1,160 @@
+CREATE OR REPLACE FUNCTION get_explorer_home
+(
+  _sort_by                                        text,
+  _sort_direction                                 text,
+  _page_number                                    integer,
+  _page_size                                      integer,
+  _date_from                                      TIMESTAMP WITH TIME ZONE,
+  _date_to                                        TIMESTAMP WITH TIME ZONE,
+  _networks                                       numeric[],
+  _contracts                                      text[],
+  _event_name_like                                text,
+  _transaction_sender_like                        text
+)
+RETURNS TABLE
+(
+  id                                              uuid,
+  chain_id                                        uint256,
+  date                                            TIMESTAMP WITH TIME ZONE,
+  event_name                                      text,
+  transaction_sender                              address,
+  page_size                                       integer,
+  page_number                                     integer,
+  total_records                                   integer,
+  total_pages                                     integer
+)
+STABLE
+AS
+$$
+  DECLARE _total_records                          integer;
+  DECLARE _total_pages                            integer;
+  DECLARE _query                                  text;
+BEGIN
+  IF(COALESCE(_sort_direction, '') = '') THEN
+    _sort_direction := 'ASC';
+  END IF;
+  
+  IF(_sort_direction NOT IN ('ASC', 'DESC')) THEN
+    RAISE EXCEPTION 'Access is denied. Invalid sort_direction: "%"', _sort_direction; --SQL Injection Attack
+  END IF;
+
+  IF(_networks IS NULL) THEN
+    _networks := array_agg(DISTINCT core.transactions.chain_id) FROM core.transactions;
+  END IF;
+
+  IF(_contracts IS NULL) THEN
+    _contracts := array_agg(DISTINCT core.transactions.address) FROM core.transactions;  
+  END IF;
+
+  IF (_sort_by NOT IN('chain_id', 'date', 'event_name', 'coupon_code', 'transaction_sender', 'ck', 'pk')) THEN
+    RAISE EXCEPTION 'Access is denied. Invalid sort_by: "%"', _sort_by; --SQL Injection Attack
+  END IF;
+  
+  IF(_sort_by = 'date') THEN
+    _sort_by := 'block_timestamp';
+  END IF;
+      
+  IF(_page_number < 1) THEN
+    RAISE EXCEPTION 'Invalid page_number value %', _page_number;  
+  END IF;
+  
+  IF(_page_size < 1) THEN
+    RAISE EXCEPTION 'Invalid _page_size value %', _page_size;  
+  END IF;
+  
+  
+  _query := format('
+  WITH result AS
+  (
+    SELECT * FROM core.transactions
+    WHERE core.transactions.block_timestamp
+      BETWEEN EXTRACT(epoch FROM COALESCE(%L, ''1-1-1990''::date))
+      AND EXTRACT(epoch FROM COALESCE(%L, ''1-1-2990''::date))
+    AND core.transactions.chain_id = ANY(%L)
+    AND core.transactions.address = ANY(%L)
+    AND core.transactions.event_name ILIKE %s
+    AND core.transactions.transaction_sender ILIKE %s
+  )
+  SELECT COUNT(*) FROM result;', _date_from, _date_to, _networks, _contracts, quote_literal_ilike(_event_name_like), quote_literal_ilike(_transaction_sender_like));
+  
+  -- RAISE NOTICE '%', _query;
+
+  EXECUTE _query
+  INTO _total_records;
+  
+  
+  _total_pages = COALESCE(CEILING(_total_records::numeric / _page_size), 0);
+  
+   _query := format('
+    SELECT
+    core.transactions.id,
+    core.transactions.chain_id,
+    to_timestamp(core.transactions.block_timestamp)::TIMESTAMP WITH TIME ZONE AS date,
+    core.transactions.event_name,
+    core.transactions.transaction_sender,
+    %s AS page_size,
+    %s AS page_number,
+    %s AS total_records,
+    %s AS total_pages
+  FROM core.transactions
+  WHERE core.transactions.block_timestamp
+    BETWEEN EXTRACT(epoch FROM COALESCE(%L, ''1-1-1990''::date))
+    AND EXTRACT(epoch FROM COALESCE(%L, ''1-1-2990''::date))
+  AND core.transactions.chain_id = ANY(%L)
+  AND core.transactions.address = ANY(%L)
+  AND core.transactions.event_name ILIKE %s
+  AND core.transactions.transaction_sender ILIKE %s
+  ORDER BY %I %s
+  LIMIT %s::integer
+  OFFSET %s::integer * %s::integer  
+  ', _page_size, _page_number, _total_records, _total_pages, _date_from, _date_to, _networks, _contracts, quote_literal_ilike(_event_name_like), quote_literal_ilike(_transaction_sender_like), _sort_by, _sort_direction, _page_size, _page_number - 1, _page_size);
+
+  --RAISE NOTICE '%', _query;
+  RETURN QUERY EXECUTE _query;
+END
+$$
+LANGUAGE plpgsql;
+
+
+ALTER FUNCTION get_explorer_home
+(
+  text,
+  text,
+  integer,
+  integer,
+  TIMESTAMP WITH TIME ZONE,
+  TIMESTAMP WITH TIME ZONE,
+  numeric[],
+  text[],
+  text,
+  text
+) OWNER TO writeuser;
+
+
+-- DROP FUNCTION get_explorer_home
+-- (
+--   text,
+--   text,
+--   integer,
+--   integer,
+--   TIMESTAMP WITH TIME ZONE,
+--   TIMESTAMP WITH TIME ZONE,
+--   numeric[],
+--   text[],
+--   text,
+--   text
+-- );
+
+-- SELECT * FROM get_explorer_home
+-- (
+--   'date',           --_sort_by                                        text,
+--   'DESC',           --_sort_direction                                 text,
+--   1,                --_page_number                                    integer,
+--   2,                --_page_size                                      integer,
+--   NULL,             --_date_from                                      TIMESTAMP WITH TIME ZONE,
+--   '1-1-2099'::date, --_date_to                                        TIMESTAMP WITH TIME ZONE,
+--   NULL,             --_networks                                       numeric[],
+--   NULL,             --_contracts                                      text[],
+--   '',               --_event_name_like                                text,
+--   ''                --_transaction_sender_like                        text
+-- );
---
+++ sql/explorer/00-functions/get_explorer_stats.sql
@@ -0,0 +1,54 @@
+DROP FUNCTION IF EXISTS get_explorer_stats();
+
+CREATE FUNCTION get_explorer_stats()
+RETURNS TABLE
+(
+  transaction_count                                 integer,
+  total_swaps                                       numeric,
+  liquidity_added                                   numeric,
+  liquidity_removed                                 numeric
+)
+AS
+$$
+BEGIN
+  DROP TABLE IF EXISTS _get_explorer_stats_result;
+  CREATE TEMPORARY TABLE _get_explorer_stats_result
+  (
+    transaction_count                                 integer,
+    total_swaps                                       numeric,
+    liquidity_added                                   numeric,
+    liquidity_removed                                 numeric
+  ) ON COMMIT DROP;
+  
+  INSERT INTO _get_explorer_stats_result(transaction_count)
+  SELECT COUNT(*)
+  FROM core.transactions;
+  
+  UPDATE _get_explorer_stats_result
+  SET total_swaps = 
+  COALESCE((
+    SELECT SUM(get_stablecoin_value(swap_transaction_view.chain_id, swap_transaction_view.stablecoin_amount))
+    FROM swap_transaction_view
+  ), 0);
+  
+  UPDATE _get_explorer_stats_result
+  SET liquidity_added =
+  COALESCE((
+    SELECT SUM(get_stablecoin_value(liquidity_transaction_view.chain_id, liquidity_transaction_view.stablecoin_amount))
+    FROM liquidity_transaction_view
+    WHERE action='add'
+  ), 0);
+  
+  UPDATE _get_explorer_stats_result
+  SET liquidity_removed =
+  COALESCE((
+    SELECT SUM(get_stablecoin_value(liquidity_transaction_view.chain_id, liquidity_transaction_view.stablecoin_amount))
+    FROM liquidity_transaction_view
+    WHERE action='remove'
+  ), 0);
+  
+  RETURN QUERY
+  SELECT * FROM _get_explorer_stats_result;
+END
+$$
+LANGUAGE plpgsql;
